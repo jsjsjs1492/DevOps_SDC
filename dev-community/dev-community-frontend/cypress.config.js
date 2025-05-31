9522e5348aacd716f09dff49a06fe5ef69ebfeef
jsjsjs1492 <jesjsjes@naver.com>
2025-05-31 22:07:29 +0900

    ....

--- dev-community/dev-community-frontend/cypress.config.js
+++ dev-community/dev-community-frontend/cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://frontend:80',
-    specPattern: 'e2e/**/*.cy.js',
-    supportFile: 'support/e2e.js',
+    specPattern: 'cypress/e2e/**/*.cy.js',
+    supportFile: 'cypress/support/e2e.js',
   },
 })
