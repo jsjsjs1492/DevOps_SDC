94aec27edb2525265e87ee2d54e4fa34eca85fc3
jsjsjs1492 <jesjsjes@naver.com>
2025-06-07 21:49:08 +0900

    ㅇㅇ

--- dev-community/dev-community-backend/src/main/java/com/letsgo/devcommunity/global/config/SecurityConfig.java
+++ dev-community/dev-community-backend/src/main/java/com/letsgo/devcommunity/global/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("13.124.40.201:5002")); // 프론트 주소
+        configuration.setAllowedOrigins(List.of("http://13.124.40.201:5002")); // 프론트 주소
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
